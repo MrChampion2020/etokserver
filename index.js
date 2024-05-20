bef577b118fc73f38cd8e68f7cd6bff664af8d7b
runorx <145400827+runorx@users.noreply.github.com>
2024-05-20 09:48:15 +0100

    merge remote changes

--- index.js
+++ index.js
@@ -318,7 +318,7 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const cors = require("cors");
 
 const http = require("http").createServer(app);
